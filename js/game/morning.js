17616c31cc350449c94b0af9506e49ccf0f217a7
Benjamin Landers <benjaminrlanders@gmail.com>
2015-05-17 15:03:16 -0700

    finished morning (mostly)

--- js/game/morning.js
+++ js/game/morning.js
@@ -1,33 +1,62 @@
 function Start(){
-
 	$ = {};
-	
-	/////// SET UP SCENE ////////
-
-	//PlaySound("bg","coffeehouse",{loop:-1, volume:0.7});
-
-	//////////////////////////////
 
 	narrator("<b>[Untitled]</b>");
 	narrator("A game about life.");
 	narrator("Are you ready to begin?");
-  Wakeup();
 	Choose({
-		"Yes": Wakeup,
-		"No": Wakeup
+		"Yes": function(m){player(m);Clear();Wakeup()},
+		"No": function(m){player(m);Clear();Wakeup()}
 	});
-
 }
 
-function Wakeup(m){
-  if(m){
-    
+function Wakeup(){
+  $.temp = {};
+  if($.loop){
+    $.loop++;
+  }else{
+    $.loop = 1;
   }
   Show("background","alarm");
-  PlaySound("bg","alarm",{loop:0, volume:0.7});
-  narrator("Hopefully, you can find yours");
+  PlaySound("alarm","alarm",{loop:0, volume:0.7});
+  narrator("*Alarm Sounds*");
   Choose({
-		"Ok": function(m){},
-		"Later, Mom": function(m){}
+		"*Wake up*": function(m){player(m);PlaySound("alarm","alarm",{loop:0, volume:0});Clear();narrator("You get out of bed");MorningRoutine();}
 	});
+}
+function MorningRoutine(){
+  //set room art
+	if($.temp.dressed){
+	  Choose({
+		  "*leave*": function(m){player(m);Clear();LeaveHome();}
+	  });
+	}else{
+	  Choose({
+		  "*Get Dressed*": function(m){player(m);$.temp.dressed = true;MorningRoutine();}
+	  });
+	}
+}
+function LeaveHome(){
+	narrator("You walk out the front door");
+  if($.loop <= 2){
+  	narrator("*SPLASH*");
+  	narrator("A car driving by hit a puddle and drenched you in disgusting water");
+  	$.temp.drenched  = true;
+  	if($.loop == 2){
+  	  thought("Wait");
+  	  thought("...");
+  	  thought("What are the odds this would happen twice in a row?");
+  	}
+  	Choose({
+		  "*Go back home and change clothes*": function(m){player(m);narrator('After changing clothes, you begin your day as usual.');EndMorning()}
+	  });
+	  return;
+	}else{
+	  EndMorning();
+	}
+	
+}
+function EndMorning(){
+  Clear();
+	StartWork();
 }
